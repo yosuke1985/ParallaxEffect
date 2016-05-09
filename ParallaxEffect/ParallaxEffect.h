78841a6533c990a5ee2de95f20525af08ed65c9e
中山 陽介 <ihatenonoildressing@gmail.com>
2016-05-09 11:32:29 +0900

    Initial commit

---
+++ ParallaxEffect/ParallaxEffect.h
@@ -0,0 +1,19 @@
+//
+//  ParallaxEffect.h
+//  ParallaxEffect
+//
+//  Created by 中山 陽介 on 5/9/16.
+//  Copyright © 2016 Yosuke Nakayama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParallaxEffect.
+FOUNDATION_EXPORT double ParallaxEffectVersionNumber;
+
+//! Project version string for ParallaxEffect.
+FOUNDATION_EXPORT const unsigned char ParallaxEffectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParallaxEffect/PublicHeader.h>
+
+
